0c7879a1f460727ff5dc0fdcc5af4c10cd43772c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-14 16:55:59 +0200

    v2.01

---
+++ 1.4/Source/VAECaves/HarmonyPatches/MapTemperature_SeasonAcceptableFor_Patch.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+using Verse.AI;
+
+namespace VAECaves
+{
+
+    [HarmonyPatch(typeof(MapTemperature))]
+    [HarmonyPatch("SeasonAcceptableFor")]
+    public static class MagicalMenagerie_MapTemperature_SeasonAcceptableFor_Patch
+    {
+        [HarmonyPostfix]
+        public static void AllowAnimalSpawns(ThingDef animalRace, ref bool __result, Map ___map)
+
+        {
+
+            if(animalRace != null && animalRace.defName.Contains("VAECaves_"))
+                    {
+                if (!Find.World.HasCaves(___map.Tile))
+                {
+                    __result= false;
+                }
+
+            }
+
+           
+
+
+        }
+    }
+
+
+
+
+
+}
